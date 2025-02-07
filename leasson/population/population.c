66b30e7faeb7b71a2182b5d9156add16dc772ff6
rduarte12 <duarterafael533@gmail.com>
2025-02-07 13:35:09 -0300

    Update population.c

--- leasson/population/population.c
+++ leasson/population/population.c
@@ -2,43 +2,44 @@
 #include <math.h>
 #include <stdlib.h>
 
-int popi = 0; //initial population
-int popf = 0; //final population
+int popi = 0; // Initial population
+int popf = 0; // Final population
 int years = 0;
-
 
 int main(void)
 {
-    //input of initial and final population
+    // Input for initial and final population
     do
     {
-    printf("Initial population: \n");
-    scanf("%d", &popi);
-    }while(popi < 9);
+        printf("Initial population: \n");
+        scanf("%d", &popi);
+    } while (popi < 9); // Ensures the initial population is at least 9
 
     do
     {
-    printf("Final population: \n");
-    scanf("%d", &popf);
-    }while(popf < popi);
+        printf("Final population: \n");
+        scanf("%d", &popf);
+    } while (popf < popi); // Ensures the final population is not smaller than the initial population
 
-    if(popf == popi)
+    // If the initial and final populations are the same, no years are needed
+    if (popf == popi)
     {
         printf("The population will remain the same, so it will take 0 years.\n");
-    }else{
-
-    int d = (int)floor(popf / popi);
-
-    years = (log(d)/log(1.083));
-
-    if(years < 1){
-        years++;
-        printf("Answer: %d years\n\n", years);
-    }else{
-        printf("Answer: %d years\n\n", years);
     }
+    else
+    {
+        int d = (int)floor(popf / popi); // Calculates the population growth ratio
+        years = (log(d) / log(1.083)); // Computes the number of years based on a growth rate of 8.3%
 
+        // Ensures at least one year is counted if the computed value is less than 1
+        if (years < 1)
+        {
+            years++;
+            printf("Answer: %d years\n\n", years);
+        }
+        else
+        {
+            printf("Answer: %d years\n\n", years);
+        }
     }
-
-
 }
